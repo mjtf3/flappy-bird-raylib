fb7d92b89439c57c823ac0b7b0317108e2f7b320
Manuel José Tornero Fuster <91553219+mjtf3@users.noreply.github.com>
2025-09-14 15:46:18 +0200

    Initial commit

---
+++ src/MainMenuState.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <GameState.hpp>
+
+class MainMenuState : public GameState
+{
+    public:
+        MainMenuState();
+        ~MainMenuState() = default;
+
+        void init() override;
+        void handleInput() override;
+        void update(float deltaTime) override;
+        void render() override;
+
+        void pause(){};
+        void resume(){};
+
+    
+    private:
+        char entered_key;
+};
